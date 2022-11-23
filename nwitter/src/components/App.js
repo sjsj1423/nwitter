fd632d6843752a1238d9e585630dd258804facb6
KIMKIM <kimphilbit@gmail.com>
2022-11-23 19:57:50 +0900

    1.0 Initialization

--- nwitter/src/components/App.js
+++ nwitter/src/components/App.js
@@ -8,11 +8,13 @@ import userEvent from "@testing-library/user-event";
 function App() {
   const [init, setInit] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userObj, setUserObj] = useState(null);
 
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
       if(user) {
         setIsLoggedIn(user);
+        setUserObj(user); 
       } else {
         setIsLoggedIn(false);
       }
@@ -21,7 +23,7 @@ function App() {
   }, []);
     return (
     <>
-    {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "initializing..."};
+    {init ? <AppRouter isLoggedIn={isLoggedIn} userObj={userObj}/> : "initializing..."};
     <footer>&copy; {new Date().getFullYear()} Nwitter </footer>
     </>
     )
--- nwitter/src/routes/Home.js
+++ nwitter/src/routes/Home.js
@@ -1,19 +1,20 @@
 import { dbService } from "fbase";
 import { useEffect, useState } from "react";
+import Nweet from "components/Nweet";
 
-const Home = () => {
+const Home = ({ userObj }) => {
     const [nweet, setNweet] = useState("");
     const [nweets, setNweets] = useState([]);
     
-    const getNweets = async () => {
-        const dbNweets = await dbService.collection("nweets").get();
-        dbNweets.forEach((document) => { const nweetObject = {...document.data(), id: document.id };
-        setNweets((prev)=> [nwwetObject, ...prev])}
-        );
-    };
 
     useEffect(() => {
-        getNweets();
+        dbService.collection("nweets").onSnapshot((snapshot)=> {
+            const newArray = snapshot.docs.map((document) => ({
+                id: document.id,
+                ...document.data(),
+            }));
+            setNweets(newArray);
+        });
     }, []);
 
     const onSubmit = async (event) => {
@@ -21,6 +22,7 @@ const Home = () => {
         await dbService.collection("nweets").add({
             text: nweet,
             createdAt: Date.now(),
+            creatorId: userObj.uid,
         });
         setNweet("");
     };
@@ -48,9 +50,7 @@ const Home = () => {
         </form>
         <div>
             {nweets.map((nweet)=> (
-                <div key={nweet.id}>
-                    <h4>{nweet.text}</h4>
-                </div>
+                <Nweet key={nweet.id} nweetObj={nweet} isOwner={nweet.creatorId === userObj.uid}/>
             ))};
         </div>
         </>
